f11081cbc67d7a681de806181bf6bf2c1c1b6fb4
kmm2204 <krista.mar@ischool.berkeley.edu>
2017-04-11 17:29:38 -0400

    updated wordcount.py

--- 205_hw/exercise_2_new/extweetwordcount/src/bolts/wordcount.py
+++ 205_hw/exercise_2_new/extweetwordcount/src/bolts/wordcount.py
@@ -1,4 +1,4 @@
-rom __future__ import absolute_import, print_function, unicode_literals
+from __future__ import absolute_import, print_function, unicode_literals
 
 from collections import Counter
 from streamparse.bolt import Bolt
@@ -10,28 +10,32 @@ class WordCounter(Bolt):
 
     def initialize(self, conf, ctx):
         self.counts = Counter()
-        self.conn = psycopg2.connect(database="postgres", user="postgres", password="pass", host="localhost", port="5432")
 
 
     def process(self, tup):
         word = tup.values[0]
+        self.counts[word] +=1
+        #connect to database
+        connector = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
 
-        cur = self.conn.cursor()
-        cur.execute("UPDATE tweetwordcount SET count=count+1 WHERE word=%s", (word,))
+        cur = connector.cursor()
 
-        # Write codes to increment the word count in Postgres
-        # Use psycopg to interact with Postgres
-        # Database name: Tcount
-        # Table name: Tweetwordcount
-        # you need to create both the database and the table in advance.
+        #see if our word exists or not in the databse
+        cur.execute("SELECT word,count FROM tweetwordcount WHERE word=%s", [word])
+        record = cur.fetchone()
 
-        #create a string
-        word=str(word)
-        
-        
-        # Increment the local count
-        self.counts[word] += 1
+        #create a record if there is none
+        if record is None:
+                cur.execute("INSERT INTO tweetwordcount (word,count) VALUES (%s,%s)", (word, 1))
+                connector.commit()
+        else:
+             	cur.execute("UPDATE tweetwordcount SET count=%s WHERE word=%s;", (self.counts[word], word))
+                connector.commit()
+
+        # Emit the word and count
         self.emit([word, self.counts[word]])
 
         # Log the count - just to see the topology running
         self.log('%s: %d' % (word, self.counts[word]))
+
+
